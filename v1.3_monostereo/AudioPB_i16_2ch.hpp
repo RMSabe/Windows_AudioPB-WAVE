b63ff211da90a6d1eea4b34245f26d9dd029bcbb
RMSabe <69878824+RMSabe@users.noreply.github.com>
2025-10-04 19:13:57 -0500

    Add files via upload

---
+++ v1.3_monostereo/AudioPB_i16_2ch.hpp
@@ -0,0 +1,26 @@
+/*
+	WAVE Audio Playback for Windows
+	Version 1.3 (mono-stereo)
+
+	Author: Rafael Sabe
+	Email: rafaelmsabe@gmail.com
+*/
+
+#ifndef AUDIOPB_I16_2CH_HPP
+#define AUDIOPB_I16_2CH_HPP
+
+#include "AudioPB.hpp"
+
+class AudioPB_i16_2ch : public AudioPB {
+	public:
+		AudioPB_i16_2ch(const audiopb_params_t *p_params);
+		~AudioPB_i16_2ch(VOID);
+
+	private:
+		BOOL WINAPI audio_hw_init(VOID) override;
+		BOOL WINAPI buffer_alloc(VOID) override;
+		VOID WINAPI buffer_free(VOID) override;
+		VOID WINAPI buffer_load(VOID) override;
+};
+
+#endif /*AUDIOPB_I16_2CH_HPP*/
---
+++ v1.3_monostereo/AudioPB_i24_1ch.hpp
@@ -0,0 +1,30 @@
+/*
+	WAVE Audio Playback for Windows
+	Version 1.3 (mono-stereo)
+
+	Author: Rafael Sabe
+	Email: rafaelmsabe@gmail.com
+*/
+
+#ifndef AUDIOPB_I24_1CH_HPP
+#define AUDIOPB_I24_1CH_HPP
+
+#include "AudioPB.hpp"
+
+class AudioPB_i24_1ch : public AudioPB {
+	public:
+		AudioPB_i24_1ch(const audiopb_params_t *p_params);
+		~AudioPB_i24_1ch(VOID);
+
+	private:
+		SIZE_T BYTEBUF_SIZE = 0u;
+
+		UINT8 *p_bytebuf = NULL;
+
+		BOOL WINAPI audio_hw_init(VOID) override;
+		BOOL WINAPI buffer_alloc(VOID) override;
+		VOID WINAPI buffer_free(VOID) override;
+		VOID WINAPI buffer_load(VOID) override;
+};
+
+#endif /*AUDIOPB_I24_1CH_HPP*/
